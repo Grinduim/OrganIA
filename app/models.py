3a7f3f1ec4ddd5e3708ba9aa7fe3ac22a29059f6
Grinduim <viniciusgribelperson@gmail.com>
2024-10-05 23:10:23 -0300

    ➕ Basic file structure  + basic files for fastAPI

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Date
+from app.db import Base
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    date = Column(Date)
+    review = Column(String)
+    sentiment = Column(String)
